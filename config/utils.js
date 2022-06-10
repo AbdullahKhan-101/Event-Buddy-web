2b807ecc5af58ecb77d23d2e651bd315cd6d54e1
farzam <farzamyousuf1310@gmail.com>
2022-06-11 03:05:39 +0500

    Debug

--- config/utils.js
+++ config/utils.js
@@ -1,6 +1,6 @@
 // export const baseUrl = "http://54.144.168.52:3000/";
 export const baseUrl = "http://api.theeventbuddy.com/";
-export const SOCKET_URL = "http://54.144.168.52:3000/";
+export const SOCKET_URL = "http://api.theeventbuddy.com/";
 const actorId = "64";
 const authToken = "fa1411c4-ac3e-4f96-8692-d3279177e296";
 export let socket = null;
