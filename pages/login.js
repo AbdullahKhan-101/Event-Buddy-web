deacbe6dbc77a719c4cc3e9bcefdfff66dcf526a
abdullahkhan44 <86144086+abdullahkhan44@users.noreply.github.com>
2022-06-14 07:50:06 -0700

    add background imgages

--- pages/login.js
+++ pages/login.js
@@ -14,24 +14,13 @@ const Signin = () => {
 
   console.log("--->login.js");
   return (
-    <div>
+    <div
+    //  className="bg-[url('/bgdesign1.png')] w-[100%] h-[100%] border"
+    >
+      {/* <h1>hello</h1> */}
       <Login />
-      <div className="absolute top-0 md:h-[131px] h-[106px] w-52 md:w-64 ">
-        <Image
-          src="/bgdesign1.png"
-          alt="infoImg"
-          layout="fill"
-          objectfit="contain"
-        />
-      </div>
-      <div className="absolute bottom-0 right-0 hidden h-[107px] -mt-3 w-64  md:block ">
-        <Image
-          src="/bgdesign2.png"
-          alt="infoImg"
-          layout="fill"
-          objectfit="contain"
-        />
-      </div>
+      <div className="absolute bg-[url('/bgdesign1.png')] top-0 md:h-[131px] h-[106px] w-52 md:w-64  bg-no-repeat"></div>
+      <div className="absolute bottom-0 right-0 hidden h-[107px] bg-[url('/bgdesign2.png')] -mt-3 w-64  md:block bg-no-repeat"></div>
     </div>
   );
 };
--- pages/privacy_policy.js
+++ pages/privacy_policy.js
@@ -6,22 +6,8 @@ const privacyPolicys = () => {
   return (
     <div>
       <PrivacyPolicyM />
-      <div className="absolute top-0 md:h-[131px] h-[106px] w-52 md:w-64 ">
-        <Image
-          src="/bgdesign1.png"
-          alt="infoImg"
-          layout="fill"
-          objectfit="contain"
-        />
-      </div>
-      <div className="absolute bottom-0 right-0 hidden h-[107px] -mt-3 w-64  md:block ">
-        <Image
-          src="/bgdesign2.png"
-          alt="infoImg"
-          layout="fill"
-          objectfit="contain"
-        />
-      </div>
+      <div className="absolute bg-[url('/bgdesign1.png')] top-0 md:h-[131px] h-[106px] w-52 md:w-64  bg-no-repeat"></div>
+      <div className="absolute bottom-0 right-0 hidden h-[107px] bg-[url('/bgdesign2.png')] -mt-3 w-64  md:block bg-no-repeat"></div>
     </div>
   );
 };
